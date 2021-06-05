4a34626fc5e06f6167a18bec0ce4a3f12dc0169e
Sherzod Usmonov <sherzod.usmon.91@gmail.com>
2021-06-06 01:44:25 +0500

    make simple bot

---
+++ app/helpers/BotHelp.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Helpers;
+
+use Exception;
+
+class BotHelp 
+{
+    public static function cLog($txt)
+    {
+        global $telegram;
+        try {
+            $telegram->sendMessage([
+                'chat_id' => 122420625, 
+                'text' => json_encode($txt),
+                'parse_mode' => "Markdown",
+            ]);
+        } catch (Exception $e) {
+            error_log($e->getMessage());
+        }
+    }
+}
+
+
+?>
---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+// make error enable
+error_reporting( E_ALL );
+ini_set( "display_errors", 1 );
+define('TIMEZONE', 'Asia/Tashkent');
+date_default_timezone_set(TIMEZONE);
+define('TOKEN', '1882359300:AAGEJ2X2FfE-u6Q_QbY8BeYtqgSRUyJKQL8');
+
+//load vendor
+require 'vendor/autoload.php';
+
+// use libs 
+
+use App\Helpers\BotHelp;
+use Telegram\Bot\Api;
+
+// init telegram api
+$telegram = new Api(TOKEN);
+$updates = $telegram->getWebhookUpdates();
+
+$message = null; 
+$callback = null;
+$channel = null;
+
+if ($updates->getMessage() != null) {
+    $message = $updates->getMessage();
+} else if ($updates->getEditedMessage() != null) {
+    $message = $updates->getEditedMessage();
+} else if ($updates->getCallbackQuery() != null) {
+    $callback = $updates->getCallbackQuery();
+} else if ($updates->getChannelPost() !== null) {
+    $channel = $updates->getChannelPost();
+}
+
+// message is not null
+if (!is_null($message)) {
+    // get object
+    $text = $message->getText();
+    $user = $message->getFrom();
+    $chat = $message->getChat();
+    $location = $message->getLocation();
+    $contact = $message->getContact();
+
+    // if user is private so answer to him
+    if (strtolower($chat->getType()) === "private") {
+        // sqlite connection
+        // $connect = new SQLiteHelper();
+        // $lang = $connect->getLang($user->getId());
+
+        // if text command exists so answer to him
+        if (!empty($text) && !$user->isBot()) {
+            try {
+                $telegram->sendMessage([
+                    'chat_id' => $chat->getId(), 
+                    'text' => "Hello",
+                    'parse_mode' => "Markdown",
+                ]);
+            } catch (Exception $e) {
+                BotHelp::cLog($e->getMessage());
+            }
+        }
+
+    } elseif (strtolower($chat->getType()) == ("group" || "supergroup")) {
+        if (strtolower($text) === "/mychatid") {
+            //
+        }
+
+    }
+
+} elseif (!is_null($callback)) {
+    $message = $callback->getMessage();
+    $from = $callback->getFrom();
+    $data = $callback->getData();
+
+    if (!is_null($message) && !empty($data)) {
+        $chat = $message->getChat();
+        $decode = json_decode($data);
+        // 
+    }
+
+} elseif (!is_null($channel)) {
+    $text = $channel->getText();
+    if (strtolower($text) === "/mychatid") {
+        // 
+    }
+
+}
+
+header ( 'Content-Type: application/json' );
+echo '{"ok":true,"result":true}';
+exit ();
+
+
+?>
